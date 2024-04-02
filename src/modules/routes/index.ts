fea0ea6c71f0ed44c6514e0edc35b1d8faee70dc
Arnab Chaudhuri <arnab.jis.it@gmail.com>
2024-04-03 01:13:46 +0530

    added prettier formatting

--- src/modules/routes/index.ts
+++ src/modules/routes/index.ts
@@ -1,8 +1,14 @@
-import express from 'express';
-import { anonymousRoute } from './anonymousRoute';
-import { userRoute } from './userRoute';
-import { uiRoute } from './uiRoute';
-import { roleRoute } from './roleRoute';
+/**
+ * Configures the routes for the Express app.
+ *
+ * Imports route handlers from other files and attaches them to the router.
+ * Exports the configured router.
+ */
+import express from "express";
+import { anonymousRoute } from "./anonymousRoute";
+import { userRoute } from "./userRoute";
+import { uiRoute } from "./uiRoute";
+import { roleRoute } from "./roleRoute";
 
 export const routes = express.Router();
 
--- src/modules/routes/roleRoute.ts
+++ src/modules/routes/roleRoute.ts
@@ -1,35 +1,47 @@
-import { NextFunction, Request, Response, Router } from 'express';
-import { AuthenticationService } from '../base/services/Authentication';
-import Logger from '../logger/Logger';
-import { RESPONSE_ERROR, ResponseJSON, RouteUri } from './const';
-import { BearerData } from '../base/const';
-import { RoleEntity } from '../base/entity/Role';
-import { UserEntity } from '../base/entity/User';
-import { CustomMiddleware } from './customMiddleware';
+/**
+ * Gets all roles from the database.
+ *
+ * 1. Authenticates the request using a middleware.
+ * 2. Authorizes the request using a middleware.
+ * 3. Gets all roles from the RoleEntity and returns them.
+ */
+import { NextFunction, Request, Response, Router } from "express";
+import { ResponseJSON } from "./const";
+import { RoleEntity } from "../base/entity/Role";
+import { CustomMiddleware } from "./customMiddleware";
 
 export const roleRoute = Router();
 
 const middleware = new CustomMiddleware();
 middleware.setRouteProps("/role/getAll");
-roleRoute.get("/role/getAll", async (req:Request, res:Response, next:NextFunction) => { 
+roleRoute.get(
+  "/role/getAll",
+  async (req: Request, res: Response, next: NextFunction) => {
     const authenticated = await middleware.authenticationMiddleware(req, res);
-    if(authenticated){
-        next();
+    if (authenticated) {
+      next();
     } else {
-        res.status(401).json({error:{message: "authentication failure"}} as ResponseJSON);
+      res
+        .status(401)
+        .json({ error: { message: "authentication failure" } } as ResponseJSON);
     }
-    
-}, 
+  },
 
-async (req:Request, res:Response, next:NextFunction) => { 
+  async (req: Request, res: Response, next: NextFunction) => {
     const authorized = await middleware.authorizationMiddleware(req, res);
-    if(authorized){
-        next();
+    if (authorized) {
+      next();
     } else {
-        res.status(401).json({error: {message:"user don't have right permission"}, data:null} as ResponseJSON);
+      res
+        .status(401)
+        .json({
+          error: { message: "user don't have right permission" },
+          data: null,
+        } as ResponseJSON);
     }
-    
-}, async (req:Request, res:Response) => {
+  },
+  async (req: Request, res: Response) => {
     const roles = await RoleEntity.getAllRoles();
     res.status(200).json(roles);
-});
+  }
+);
